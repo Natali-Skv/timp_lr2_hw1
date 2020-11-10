e9caa6c3aa5aa27a933f95c2d8c59d7976b8510d
Natali-Skv <nat-s.skv@mail.ru>
2020-11-11 01:11:25 +0300

    added coomments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 int main() 
-{ 
+{
+    // printing "Hello word from <name>"
     std::cout << "What is yourname? ";
     std::string name;
+    //only one word will be written to "name"
     std::cin >> name;
     std::cout << "Hello world from "<< name<<std::endl;
     return 0;
