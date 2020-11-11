f625b412c16261850d4e36dd7ccc362c35209564
Natali-Skv <nat-s.skv@mail.ru>
2020-11-11 05:00:21 +0300

    reformated code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
-int main() 
-{
-    // this code meant for printing "Hello word from <name>"
+int main() {
+    // printing "Hello word from <name>"
     std::cout << "What is yourname? ";
     std::string name;
-    //Attention!!!only one word will be written to "name"
+    //only one word will be written to "name"
     std::cin >> name;
-    std::cout << "Hello world from "<< name<<std::endl;
+    std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
