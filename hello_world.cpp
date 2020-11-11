f754ff7415dbda3d0870d786928e3da247c986ee
Natalia Skvortsova <71991158+Natali-Skv@users.noreply.github.com>
2020-11-11 15:32:06 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,10 @@
 
 int main() 
 {
-    // printing "Hello word from <name>"
+    // this code meant for printing "Hello word from <name>"
     std::cout << "What is yourname? ";
     std::string name;
-    //only one word will be written to "name"
+    //Attention!!!only one word will be written to "name"
     std::cin >> name;
     std::cout << "Hello world from "<< name<<std::endl;
     return 0;
